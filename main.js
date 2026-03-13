33b3d99d9c173b5e9bf7acc8d90d21f1dd6a456a
vfedotova418-png <vfedotova418@gmail.com>
2026-03-13 09:45:25 +0300

    Step 9: strict and loose comparison

--- main.js
+++ main.js
@@ -95,26 +95,46 @@
 // newPrice = "Тридцать четыре девяносто девять";
 // console.log(typeof newPrice, newPrice);
 
-let num = 42;
-let str = String(num);
-let str2 = num.toString();
-let str3 = "" + num;
+// let num = 42;
+// let str = String(num);
+// let str2 = num.toString();
+// let str3 = "" + num;
 
-let strNum = "123";
-let int = Number(strNum);
-let int2 = parseInt("42.5");
-let float = parseFloat("3.14");
-let int3 = +"99";
+// let strNum = "123";
+// let int = Number(strNum);
+// let int2 = parseInt("42.5");
+// let float = parseFloat("3.14");
+// let int3 = +"99";
 
-let bool1 = Boolean(1);
-let bool2 = !!1;
-let bool3 = Boolean(0);
-let bool4 = Boolean("");
+// let bool1 = Boolean(1);
+// let bool2 = !!1;
+// let bool3 = Boolean(0);
+// let bool4 = Boolean("");
 
-console.log("5" + 3);
-console.log("5" - 3);
-console.log("5" * "2");
-console.log(true + 1);
-console.log(false + 1);
-console.log(null + 1);
-console.log(undefined + 1);
+// console.log("5" + 3);
+// console.log("5" - 3);
+// console.log("5" * "2");
+// console.log(true + 1);
+// console.log(false + 1);
+// console.log(null + 1);
+// console.log(undefined + 1);
+
+console.log(5 == "5");
+console.log(5 === "5");
+console.log(0 == false);
+console.log(0 === false);
+console.log(null == undefined);
+console.log(null === undefined);
+
+let obj1 = { name: "John" };
+let obj2 = { name: "John" };
+
+console.log(obj1 == obj2);
+console.log(obj1 === obj2);
+
+let obj3 = obj1;
+console.log(obj1 === obj3);
+
+let arr1 = [1, 2, 3];
+let arr2 = [1, 2, 3];
+console.log(arr1 === arr2);
