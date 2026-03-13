180680a9627083d497b08f8e61252b2e49a97b43
vfedotova418-png <vfedotova418@gmail.com>
2026-03-13 10:11:37 +0300

    Step 12: conditional statements in JavaScript

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-// console.log("Hello, JavaScript!");
+console.log("Hello, JavaScript!");
 
-// let age = 18;
-// let name = "Helel";
-// let isStudent = true;
+let age = 18;
+let name = "Helel";
+let isStudent = true;
 
-// console.log("Name: ", name);
-// console.log("Age: ", age);
-// console.log("Is student: ", isStudent);
+console.log("Name: ", name);
+console.log("Age: ", age);
+console.log("Is student: ", isStudent);
 
 // let value = 10;
 // console.log(value);
@@ -139,7 +139,70 @@
 // let arr2 = [1, 2, 3];
 // console.log(arr1 === arr2);
 
-let a2 = 2;
-let b2 = 3;
-console.log(a2 + b2);
-console.log(a2 * b2);
+// let a2 = 2;
+// let b2 = 3;
+// console.log(a2 + b2);
+// console.log(a2 * b2);
+
+let yourAge = 18;
+if (yourAge >= 18) {
+    console.log("Доступ разрешён");
+} else {
+    console.log("Доступ запрещён");
+}
+
+let temperature = -2;
+if (temperature < 0) {
+    console.log("Холодно");
+} else if (temperature >= 0 && temperature <= 20) {
+    console.log("Прохладно");
+} else {
+    console.log("Тепло");
+}
+
+if (isStudent && age < 25) {
+    console.log("Доступна студенческая скидка");
+}
+
+let isLoggedIn = true;
+let isAdmin = true;
+if (isLoggedIn && isAdmin) {
+    console.log("Полный доступ");
+} else if (isLoggedIn && !isAdmin) {
+    console.log("Ограниченый доступ");
+} else {
+    console.log("Доступ запрещён");
+}
+
+let a3 = 10;
+let b3 = "10";
+console.log(a3 == b3);
+console.log(a3 === b3);
+
+let message = age >= 18 ? "Соверщеннолетний" : "Несовершеннолетний";
+console.log(message);
+
+let day = 3;
+switch (day) {
+    case 1: console.log("Понедельник"); break;
+    case 2: console.log("Вторник"); break;
+    case 3: console.log("Среда"); break;
+    default: console.log("Неизвестный день");
+}
+
+let monthNumber = 3;
+switch (monthNumber) {
+    case 1: console.log("Январь"); break;
+    case 2: console.log("Февраль"); break;
+    case 3: console.log("Март"); break;
+    case 4: console.log("Апрель"); break;
+    case 5: console.log("Май"); break;
+    case 6: console.log("Июнь"); break;
+    case 7: console.log("Июль"); break;
+    case 8: console.log("Август"); break;
+    case 9: console.log("Сентябрь"); break;
+    case 10: console.log("Октябрь"); break;
+    case 11: console.log("Ноябрь"); break;
+    case 12: console.log("Декабрь"); break;
+    default: console.log("Неизвестный месяц");
+}
